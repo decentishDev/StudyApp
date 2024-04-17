02d4343f2e2be48207c497c0e783dd446dee643c
Matthew Lundeen <97109488+decentishDev@users.noreply.github.com>
2024-04-17 12:24:54 -0500

    create color palette!

---
+++ StudyApp/Colors.swift
@@ -0,0 +1,12 @@
+import UIKit
+struct Colors {
+    static let background = UIColor(red: 0.110, green: 0.082, blue: 0.102, alpha: 1.0)
+    static let secondaryBackground = UIColor(red: 0.220, green: 0.161, blue: 0.2, alpha: 1.0)
+    static let darkHighlight = UIColor(red: 0.231, green: 0.322, blue: 0.286, alpha: 1.0)
+    static let highlight = UIColor(red: 0.318, green: 0.596, blue: 0.447, alpha: 1.0)
+    static let lightHighlight = UIColor(red: 0.643, green: 0.706, blue: 0.580, alpha: 1.0)
+    static let text = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+}
+
+//	Reference by using: Colors.colorname
+//	Remember it returns a UIColor!
